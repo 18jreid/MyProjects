86cfa1a91fb85d1a40d49bd08c5da32bcc031dce
Justin Samuel Reid <jsreid27@aggiemail.usu.edu>
2021-05-03 14:54:44 -0600

    Created personal website, and added django so it can be hosted from whichever computer I choose.

---
+++ PersonalWebsite/mySite/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from mySite import views
+
+urlpatterns = [
+    path('', views.portfolioPage, name='portfolio'),
+    path('projects/', views.projectsPage, name='projects'),
+    path('contact/', views.contactPage, name='contact'),
+]
---
+++ PersonalWebsite/mySite/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def portfolioPage(request):
+    return render(request, 'mySite/index.html')
+
+
+def projectsPage(request):
+    return render(request, 'mySite/projects.html')
+
+
+def contactPage(request):
+    return render(request, 'mySite/contact.html')
