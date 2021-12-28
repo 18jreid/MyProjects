ccbad5fbae8ec3d1aa5b99b55c1dbf3664544bb7
18jreid <jsreid27@gmail.com>
2021-12-28 02:06:26 -0700

    Added player movement(of sorts), and player rotation

--- 3D Graphics Engine/src/main.py
+++ 3D Graphics Engine/src/main.py
@@ -6,19 +6,21 @@ from Triangle import Triangle
 from Mesh import Mesh
 import math
 import time
-import operator
+
 
 # Multiplies a vector and matrix together
 def multiplyMatrixVector(input:Vec3d, output:Vec3d, matrix:Mat4x4) -> None:
     output.setX((input.getX() * matrix.getIndex(0,0)) + (input.getY() * matrix.getIndex(1,0)) + (input.getZ() * matrix.getIndex(2,0)) + matrix.getIndex(3,0))
     output.setY((input.getX() * matrix.getIndex(0,1)) + (input.getY() * matrix.getIndex(1,1)) + (input.getZ() * matrix.getIndex(2,1)) + matrix.getIndex(3,1))
     output.setZ((input.getX() * matrix.getIndex(0,2)) + (input.getY() * matrix.getIndex(1,2)) + (input.getZ() * matrix.getIndex(2,2)) + matrix.getIndex(3,2))
-    w:float = (input.getX() * matrix.getIndex(0,3)) + (input.getY() * matrix.getIndex(1,3)) + (input.getZ() * matrix.getIndex(2,3)) + matrix.getIndex(3,3)
+    
+    w = (input.getX() * matrix.getIndex(0,3)) + (input.getY() * matrix.getIndex(1,3)) + (input.getZ() * matrix.getIndex(2,3)) + matrix.getIndex(3,3)
 
     if w != 0.0:
-        output.setX(output.getX() / w)
-        output.setY(output.getY() / w)
-        output.setZ(output.getZ() / w)
+        output.setX((output.getX()) / (w))
+        output.setY((output.getY()) / (w))
+        output.setZ((output.getZ()) / (w))
+
 
 # Draws object specified
 def drawObject(mesh, xOffset=0, yOffset=0, zOffset=0, vCamera=Vec3d(0,0,0)):
@@ -38,15 +40,15 @@ def drawObject(mesh, xOffset=0, yOffset=0, zOffset=0, vCamera=Vec3d(0,0,0)):
 
         # Add coordinate offset
         triTranslated = triRotatedZX
-        triTranslated.getV0().setZ(triRotatedZX.getV0().getZ() + zOffset)
-        triTranslated.getV1().setZ(triRotatedZX.getV1().getZ() + zOffset)
-        triTranslated.getV2().setZ(triRotatedZX.getV2().getZ() + zOffset)
-        triTranslated.getV0().setY(triRotatedZX.getV0().getY() + yOffset)
-        triTranslated.getV1().setY(triRotatedZX.getV1().getY() + yOffset)
-        triTranslated.getV2().setY(triRotatedZX.getV2().getY() + yOffset)
-        triTranslated.getV0().setX(triRotatedZX.getV0().getX() + xOffset)
-        triTranslated.getV1().setX(triRotatedZX.getV1().getX() + xOffset)
-        triTranslated.getV2().setX(triRotatedZX.getV2().getX() + xOffset)
+        triTranslated.getV0().setZ(triRotatedZX.getV0().getZ() + zOffset + vCamera.getZ())
+        triTranslated.getV1().setZ(triRotatedZX.getV1().getZ() + zOffset + vCamera.getZ())
+        triTranslated.getV2().setZ(triRotatedZX.getV2().getZ() + zOffset + vCamera.getZ())
+        triTranslated.getV0().setY(triRotatedZX.getV0().getY() + yOffset + vCamera.getY())
+        triTranslated.getV1().setY(triRotatedZX.getV1().getY() + yOffset + vCamera.getY())
+        triTranslated.getV2().setY(triRotatedZX.getV2().getY() + yOffset + vCamera.getY())
+        triTranslated.getV0().setX(triRotatedZX.getV0().getX() + xOffset + vCamera.getX())
+        triTranslated.getV1().setX(triRotatedZX.getV1().getX() + xOffset + vCamera.getX())
+        triTranslated.getV2().setX(triRotatedZX.getV2().getX() + xOffset + vCamera.getX())
 
         # Calculate normal vector for drawing of faces
         normal = Vec3d(1,1,1)
@@ -101,12 +103,14 @@ def drawObject(mesh, xOffset=0, yOffset=0, zOffset=0, vCamera=Vec3d(0,0,0)):
             triProjected.getV2().setY(triProjected.getV2().getY() * float(0.5) * screen_height)
 
             trianglesToRaster.append(triProjected)
-        
 
-        # Sort triangles by order to be drawn
-        testDraw(trianglesToRaster)
+        rasterTriangles(trianglesToRaster)
 
-def testDraw(triangles:list[Triangle]):
+
+def rasterTriangles(triangles:list[Triangle]):
+    # Sort Triangles
+    triangles.sort(key=lambda tri: (tri.getV0().getZ() + tri.getV1().getZ() + tri.getV2().getZ()) / 3)
+
     for x in triangles:
         pygame.draw.polygon(screen, (255 * x.getDp(), 255 * x.getDp() , 255 * x.getDp()), 
                 [(x.getV0().getX(), x.getV0().getY()), 
@@ -114,6 +118,8 @@ def testDraw(triangles:list[Triangle]):
                 (x.getV2().getX(), x.getV2().getY()),
                 (x.getV0().getX(), x.getV0().getY())],
                 0)
+
+        # Draw lines of triangles, for debugging
         pygame.draw.line(screen, (0,0,0),
                         (x.getV0().getX(), x.getV0().getY()),
                         (x.getV1().getX(), x.getV1().getY()), 3)
@@ -123,7 +129,9 @@ def testDraw(triangles:list[Triangle]):
         pygame.draw.line(screen, (0,0,0),
                         (x.getV2().getX(), x.getV2().getY()),
                         (x.getV0().getX(), x.getV0().getY()), 3)
-### ON USER x
+
+
+### ON USER UPDATE
 pygame.init()
 screenSize = pygame.display.Info()
 screen_width:float = screenSize.current_w * .75
@@ -159,7 +167,10 @@ cubeTriangles = [
 meshCube = Mesh()
 meshCube.loadFromObjectFile("MyPlane.obj")
 fTheta:float = 0.0
-vCamera:Vec3d = Vec3d(0, 0, 0)
+userPositionX:float = 0
+userPositionY:float = 0
+userPositionZ:float = 64
+vCamera:Vec3d = Vec3d(userPositionX, userPositionY, userPositionZ)
 
 # Projection Matrix Constants
 fNear:float = 0.1
@@ -190,26 +201,48 @@ matProj.setIndex(3,3, 0)
 startTime = time.time()
 elapsedTime = 0
 screen = pygame.display.set_mode([screen_width, screen_height])
+
+# Rotation Matrices
+matRotZ:Mat4x4 = Mat4x4()
+matRotX:Mat4x4 = Mat4x4()
+matRotY:Mat4x4 = Mat4x4()
+customRotation:float = 0
+
 running = True
+rotation = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            userPositionX += 0.25
+        if keys[pygame.K_d]:
+            userPositionX -= 0.25
+        if keys[pygame.K_w]:
+            userPositionZ -= 2
+        if keys[pygame.K_s]:
+            userPositionZ += 2
+        if keys[pygame.K_LCTRL]:
+            userPositionY += .25
+        if keys[pygame.K_LSHIFT]:
+            userPositionY -= .25
+        if keys[pygame.K_q]:
+            customRotation += .05
+        if keys[pygame.K_e]:
+            customRotation -= .05
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_x:
+                rotation = not rotation
+                startTime = time.time()
+    vCamera = Vec3d(userPositionX, userPositionY, userPositionZ)
     
-    # Drawing Area
-    screen.fill((0, 0, 0))
-
-    # Rotation Matrices
-    matRotZ:Mat4x4 = Mat4x4()
-    matRotX:Mat4x4 = Mat4x4()
-    matRotY:Mat4x4 = Mat4x4()
     fTheta = elapsedTime
-
     # Z ROT
-    matRotZ.setIndex(0,0, math.cos(fTheta))
-    matRotZ.setIndex(0,1, math.sin(fTheta))
-    matRotZ.setIndex(1,0, -math.sin(fTheta))
-    matRotZ.setIndex(1,1, math.cos(fTheta))
+    matRotZ.setIndex(0,0, math.cos(customRotation))
+    matRotZ.setIndex(0,1, math.sin(customRotation))
+    matRotZ.setIndex(1,0, -math.sin(customRotation))
+    matRotZ.setIndex(1,1, math.cos(customRotation))
 
     # X ROT
     matRotX.setIndex(1,1, math.cos(fTheta * 0.5))
@@ -223,8 +256,13 @@ while running:
     matRotY.setIndex(2,0, math.sin(fTheta * 0.5))
     matRotY.setIndex(2,2, math.cos(fTheta * 0.5))
     
-    drawObject(meshCube, 0, 0, 128, vCamera)
+    # Drawing Area
+    screen.fill((0, 0, 0))
+    if rotation == True:
+        elapsedTime = time.time() - startTime
+    else:
+        elapsedTime = elapsedTime
+    drawObject(meshCube, 0, 0, 64, vCamera)
 
     pygame.display.flip()
-    time.sleep(0.015)
-    elapsedTime = time.time() - startTime
+    time.sleep(0.01)
