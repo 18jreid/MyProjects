1b613bc9c0ab67bd2ed94c2a418b1f479f85bdd5
18jreid <jsreid27@gmail.com>
2021-12-29 08:40:25 -0700

    I DID IT BABYYYYYYY

--- 3D Graphics Engine/src/main.py
+++ 3D Graphics Engine/src/main.py
@@ -25,8 +25,8 @@ def multiplyMatrixVector(input:Vec3d, output:Vec3d, matrix:Mat4x4) -> None:
 
 # Draws object specified
 def drawObject(mesh, xOffset=0, yOffset=0, zOffset=0, vCamera=Vec3d(0,0,0)):
+    trianglesToRaster:list[Triangle] = []
     for tri in mesh.getMesh():
-        triProjected = Triangle(Vec3d(1,1,1), Vec3d(1,1,1), Vec3d(1,1,1))
         triTranslated = Triangle(Vec3d(1,1,1), Vec3d(1,1,1), Vec3d(1,1,1))
         triRotatedZ = Triangle(Vec3d(1,1,1), Vec3d(1,1,1), Vec3d(1,1,1))
         triRotatedZX = Triangle(Vec3d(1,1,1), Vec3d(1,1,1), Vec3d(1,1,1))
@@ -51,17 +51,29 @@ def drawObject(mesh, xOffset=0, yOffset=0, zOffset=0, vCamera=Vec3d(0,0,0)):
         triTranslated.getV1().setX(triRotatedZX.getV1().getX() + xOffset + vCamera.getX())
         triTranslated.getV2().setX(triRotatedZX.getV2().getX() + xOffset + vCamera.getX())
 
+        trianglesToRaster.append(triTranslated)
+
+    trianglesToRaster.sort(key=lambda x: ((x.getV0().getZ() + x.getV1().getZ() + x.getV2().getZ()) / 3), reverse=True)
+    
+
+    rasterTriangles(trianglesToRaster)
+
+
+def rasterTriangles(triangles:list[Triangle]):
+    for x in triangles:
+        triProjected = Triangle(Vec3d(1,1,1), Vec3d(1,1,1), Vec3d(1,1,1))
+        
         # Calculate normal vector for drawing of faces
         normal = Vec3d(0,0,0)
         line1 = Vec3d(0,0,0)
         line2 = Vec3d(0,0,0)
-        line1.setX(triTranslated.getV1().getX() - triTranslated.getV0().getX())
-        line1.setY(triTranslated.getV1().getY() - triTranslated.getV0().getY())
-        line1.setZ(triTranslated.getV1().getZ() - triTranslated.getV0().getZ())
+        line1.setX(x.getV1().getX() - x.getV0().getX())
+        line1.setY(x.getV1().getY() - x.getV0().getY())
+        line1.setZ(x.getV1().getZ() - x.getV0().getZ())
 
-        line2.setX(triTranslated.getV2().getX() - triTranslated.getV0().getX())
-        line2.setY(triTranslated.getV2().getY() - triTranslated.getV0().getY())
-        line2.setZ(triTranslated.getV2().getZ() - triTranslated.getV0().getZ())
+        line2.setX(x.getV2().getX() - x.getV0().getX())
+        line2.setY(x.getV2().getY() - x.getV0().getY())
+        line2.setZ(x.getV2().getZ() - x.getV0().getZ())
 
         normal.setX(line1.getY() * line2.getZ() - line1.getZ() * line2.getY())
         normal.setY(line1.getZ() * line2.getX() - line1.getX() * line2.getZ())
@@ -72,8 +84,7 @@ def drawObject(mesh, xOffset=0, yOffset=0, zOffset=0, vCamera=Vec3d(0,0,0)):
         normal.setY(normal.getY() / length)
         normal.setZ(normal.getZ() / length)
 
-        trianglesToRaster:list[Triangle] = []
-        if (normal.getX() * (triTranslated.getV0().getX() - vCamera.getX()) + normal.getY() * (triTranslated.getV0().getY() - vCamera.getY()) + normal.getZ() * (triTranslated.getV0().getZ() - vCamera.getZ()) < 0):
+        if (normal.getX() * (x.getV0().getX() - vCamera.getX()) + normal.getY() * (x.getV0().getY() - vCamera.getY()) + normal.getZ() * (x.getV0().getZ() - vCamera.getZ()) < 0):
             # Illumination
             light_direction = Vec3d(0,0, -1)
             length = math.sqrt(math.pow(light_direction.getX(), 2) + math.pow(light_direction.getY(), 2) + math.pow(light_direction.getZ(), 2))
@@ -84,9 +95,9 @@ def drawObject(mesh, xOffset=0, yOffset=0, zOffset=0, vCamera=Vec3d(0,0,0)):
             triProjected.setDp(dp)
 
             # Project triangles from 3D --> 2D
-            multiplyMatrixVector(triTranslated.getV0(), triProjected.getV0(), matProj)
-            multiplyMatrixVector(triTranslated.getV1(), triProjected.getV1(), matProj)
-            multiplyMatrixVector(triTranslated.getV2(), triProjected.getV2(), matProj)
+            multiplyMatrixVector(x.getV0(), triProjected.getV0(), matProj)
+            multiplyMatrixVector(x.getV1(), triProjected.getV1(), matProj)
+            multiplyMatrixVector(x.getV2(), triProjected.getV2(), matProj)
 
             # Scale into view
             triProjected.getV0().setX(triProjected.getV0().getX() + 1.0)
@@ -103,32 +114,23 @@ def drawObject(mesh, xOffset=0, yOffset=0, zOffset=0, vCamera=Vec3d(0,0,0)):
             triProjected.getV2().setX(triProjected.getV2().getX() * float(0.5) * screen_width)
             triProjected.getV2().setY(triProjected.getV2().getY() * float(0.5) * screen_height)
 
-            trianglesToRaster.append(triProjected)
+            pygame.draw.polygon(screen, (255 * triProjected.getDp(), 255 * triProjected.getDp() , 255 * triProjected.getDp()), 
+                    [(triProjected.getV0().getX(), triProjected.getV0().getY()), 
+                    (triProjected.getV1().getX(), triProjected.getV1().getY()),
+                    (triProjected.getV2().getX(), triProjected.getV2().getY()),
+                    (triProjected.getV0().getX(), triProjected.getV0().getY())],
+                    0)
 
-        rasterTriangles(trianglesToRaster)
-
-
-def rasterTriangles(triangles:list[Triangle]):
-    triangles.sort(key=lambda x: ((x.getV0().getZ() + x.getV1().getZ() + x.getV2().getZ()) / 3))
-
-    for x in triangles:
-        pygame.draw.polygon(screen, (255 * x.getDp(), 255 * x.getDp() , 255 * x.getDp()), 
-                [(x.getV0().getX(), x.getV0().getY()), 
-                (x.getV1().getX(), x.getV1().getY()),
-                (x.getV2().getX(), x.getV2().getY()),
-                (x.getV0().getX(), x.getV0().getY())],
-                0)
-
-        # Draw lines of triangles, for debugging
-        pygame.draw.line(screen, (0,0,0),
-                        (x.getV0().getX(), x.getV0().getY()),
-                        (x.getV1().getX(), x.getV1().getY()), 3)
-        pygame.draw.line(screen, (0,0,0),
-                        (x.getV1().getX(), x.getV1().getY()),
-                        (x.getV2().getX(), x.getV2().getY()), 3)
-        pygame.draw.line(screen, (0,0,0),
-                        (x.getV2().getX(), x.getV2().getY()),
-                        (x.getV0().getX(), x.getV0().getY()), 3)
+            # Draw lines of triangles, for debugging
+            pygame.draw.line(screen, (0,0,0),
+                            (triProjected.getV0().getX(), triProjected.getV0().getY()),
+                            (triProjected.getV1().getX(), triProjected.getV1().getY()), 3)
+            pygame.draw.line(screen, (0,0,0),
+                            (triProjected.getV1().getX(), triProjected.getV1().getY()),
+                            (triProjected.getV2().getX(), triProjected.getV2().getY()), 3)
+            pygame.draw.line(screen, (0,0,0),
+                            (triProjected.getV2().getX(), triProjected.getV2().getY()),
+                            (triProjected.getV0().getX(), triProjected.getV0().getY()), 3)
 
 
 ### ON USER UPDATE
@@ -164,8 +166,8 @@ cubeTriangles = [
     Triangle(Vec3d(1,0,1), Vec3d(0,0,0), Vec3d(1,0,0))
 ]
 
-meshSphere = Mesh()
-meshSphere.loadFromObjectFile("sphere.obj")
+myPlane = Mesh()
+myPlane.loadFromObjectFile("MyPlane.obj")
 meshCube = Mesh(cubeTriangles)
 meshCone = Mesh()
 meshCone.loadFromObjectFile("cone.obj")
@@ -265,9 +267,7 @@ while running:
         elapsedTime = time.time() - startTime
     else:
         elapsedTime = elapsedTime
-    
-    drawObject(meshSphere, 3, 0, 16, vCamera)
-    drawObject(meshCube, -3, 0, 16, vCamera)
+
+    drawObject(myPlane, 0, 0, 16, vCamera)
 
     pygame.display.flip()
-    time.sleep(0.01)
